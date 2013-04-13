2a5c89705bf475863fa506516f5205eaf3534b9a
Corey Minyard <cminyard@mvista.com>
2013-04-13 12:27:23 -0500

    blkdevremove: Add block device removal
    Add the ability to forcably remove a SCSI block device.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- fs/pnode.c
+++ fs/pnode.c
@@ -320,7 +320,8 @@ out:
  */
 static inline int do_refcount_check(struct mount *mnt, int count)
 {
-	return mnt_get_count(mnt) > count;
+	int mycount = mnt_get_count(mnt) - mnt->mnt_ghosts;
+	return (mycount > count);
 }
 
 /*
