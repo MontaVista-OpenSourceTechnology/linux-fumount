bbe54ce82663623405cc25ae737ffbdc4993de5f
Corey Minyard <cminyard@mvista.com>
2013-05-21 08:58:57 -0500

    fumount: Fix BUG_ON in __fput_sync
    __fput_sync would do a BUG if the file was still associated with
    a thread, but that's still the case in a fumount.  So don't do
    that bug if the file is in a fumount.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- fs/file_table.c
+++ fs/file_table.c
@@ -365,7 +365,10 @@ void __fput_sync(struct file *file)
 	if (atomic_long_dec_and_test(&file->f_count)) {
 		struct task_struct *task = current;
 		file_sb_list_del(file);
-		BUG_ON(!(task->flags & PF_KTHREAD));
+#ifdef CONFIG_FUMOUNT
+		if (!(file->f_mode & FMODE_FUMOUNT))
+#endif
+			BUG_ON(!(task->flags & PF_KTHREAD));
 		free_file_resources(file);
 		__fput(file);
 	}
